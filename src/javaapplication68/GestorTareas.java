02c52d3949f49ba1b2ac6b699a673a66ace135e6
Joana <yopiixf@gmail.com>
2026-03-04 15:48:37 +0000

    Inicio de Gestor de tarea colaborativa

---
+++ src/javaapplication68/GestorTareas.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication68;
+
+/**
+ *
+ * @author Joana
+ */
+public class GestorTareas {
+    public String descripcion;
+    public String completada;
+    
+    public GestorTareas(String descripcion, String completada){
+        this.descripcion = descripcion;
+        this.completada = completada;
+    
+    }
+    
+}
---
+++ src/javaapplication68/Main.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication68;
+
+/**
+ *
+ * @author Joana
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+
+
+
+
+    }
+    
+}
